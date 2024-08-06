7d1996c0a2bd5dc7132e6864e60b70ed95328153
vishnu19951295 <vishnugoel.2010@gmail.com>
2024-08-07 00:59:39 +0530

    episode 03

--- App.js
+++ App.js
@@ -1,34 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * <div id="parent">
- *      <div id ="child">
- *          <h1>This is inner H1 tag.</h1>
- *          <h2>This is H2 inner Tag!!</h2>
- *      </div>
- *       <div id ="child2">
- *          <h1>This is inner H1 tag.</h1>
- *          <h2>This is H2 inner Tag!!</h2>
- *      </div>
- * </div>
- * 
- * 
- */
-const parent = React.createElement("div",{id:"parent"},
-                [React.createElement("div",{id:"child"},
-                 [React.createElement("h1",{},"This is inner Tag for Namste React!!"),
-                    React.createElement("h2",{},"This is H2 inner Tag!!")
-                 ]),
-                 React.createElement("div",{id:"child2"},
-                    [React.createElement("h1",{},"This is inner Tag2!!"),
-                       React.createElement("h2",{},"This is H2 inner Tag2!!")
-                    ])
-                ]);
+// React.CreateElement = >Object = > Html Element(render)
 const heading = React.createElement("h1",{id:"firstElement"},"Hello World from React");
 
-console.log(heading);
+// JSX
+
+
+const elem = <span>This is span element!!</span>
+
+const Title = () => (
+  <h1 id="heading" className="head">
+    {elem}
+    This is from Title JSX!
+
+  </h1>
+  
+);
+const HeadingComponent =  () => 
+   (
+    <div id="container">
+      <Title></Title>
+      {Title()}
+      <h1>This is Functional React Component!!</h1>
+    </div>
+  );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(Title());
--- Readme.md
+++ Readme.md
@@ -4,4 +4,4 @@
 - Hosting App to a Server
 - HMR - Hot module replcaement (if we make change in any file and save it then it will reflect onto UI immediatly)
 - File watching algorithm is used for HMR.
-- 
+- Caching Faster builds ->When you save your file and observe the build time in terminal then you notice build time is decreasing it is due to parcel is caching the things. (.parcel-cache folder)
